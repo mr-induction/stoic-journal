66de15a20e4844c7f5d9ee18d5d8f28547e744f8
mr-induction <rin_phx@icloud.com>
2023-11-12 11:10:21 -0600

    Initial Commit

---
+++ StoicJournalApp/StoicJournalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoicJournalAppApp.swift
+//  StoicJournalApp
+//
+//  Created by Rin Otori on 11/12/23.
+//
+
+import SwiftUI
+
+@main
+struct StoicJournalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
